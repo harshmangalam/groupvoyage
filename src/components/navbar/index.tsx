676f8a70789d260adc2bc0d179ba1e237fc37608
harshmangalam <harshdev8218@gmail.com>
2025-02-04 11:09:08 +0530

    added combobox

--- src/components/navbar/index.tsx
+++ src/components/navbar/index.tsx
@@ -1,7 +1,14 @@
 import { LocationsCombobox } from "./locations-combobox";
 import { Logo } from "../logo";
+import { db } from "@/db/connection";
 
-export function Navbar() {
+export async function Navbar() {
+  const locations = await db.query.locationsTable.findMany({
+    columns: {
+      name: true,
+      slug: true,
+    },
+  });
   return (
     <nav className="border-b sticky top-0 bg-background z-10">
       <div className="flex items-center justify-between px-4 py-3 max-w-7xl mx-auto">
@@ -9,7 +16,7 @@ export function Navbar() {
           <Logo />
         </div>
 
-        <LocationsCombobox />
+        <LocationsCombobox locations={locations} />
       </div>
     </nav>
   );
--- src/components/navbar/locations-combobox.tsx
+++ src/components/navbar/locations-combobox.tsx
@@ -18,24 +18,32 @@ import {
   PopoverContent,
   PopoverTrigger,
 } from "@/components/ui/popover";
+import { SelectLocation } from "@/db/schema";
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
 
-const cities = [
-  { value: "new-york", label: "New York" },
-  { value: "london", label: "London" },
-  { value: "paris", label: "Paris" },
-  { value: "tokyo", label: "Tokyo" },
-  { value: "sydney", label: "Sydney" },
-  { value: "berlin", label: "Berlin" },
-  { value: "rome", label: "Rome" },
-  { value: "madrid", label: "Madrid" },
-  { value: "amsterdam", label: "Amsterdam" },
-  { value: "dubai", label: "Dubai" },
-];
-
-export function LocationsCombobox() {
+export function LocationsCombobox({
+  locations,
+}: {
+  locations: Pick<SelectLocation, "slug" | "name">[];
+}) {
   const [open, setOpen] = React.useState(false);
-  const [value, setValue] = React.useState("");
 
+  const pathname = usePathname();
+  const router = useRouter();
+  const [isPending, startTransition] = React.useTransition();
+  const searchParams = useSearchParams();
+  const city = searchParams.get("city")?.toString() || "";
+
+  function handleSelect(currentValue: string) {
+    if (currentValue === city) return;
+    const newSearchParams = new URLSearchParams(searchParams);
+    newSearchParams.set("city", currentValue);
+
+    startTransition(() => {
+      router.push(pathname + "?" + newSearchParams);
+    });
+    setOpen(false);
+  }
   return (
     <Popover open={open} onOpenChange={setOpen}>
       <PopoverTrigger asChild>
@@ -44,34 +52,34 @@ export function LocationsCombobox() {
           role="combobox"
           aria-expanded={open}
           className="w-[200px] justify-between"
+          aria-disabled={isPending}
+          disabled={isPending}
         >
-          {value
-            ? cities.find((city) => city.value === value)?.label
-            : "Search cities..."}
+          {city
+            ? locations.find((l) => l.slug === city)?.name
+            : "Search cities"}
           <ChevronsUpDown className="ml-2 h-4 w-4 shrink-0 opacity-50" />
         </Button>
       </PopoverTrigger>
       <PopoverContent className="w-[200px] p-0">
         <Command>
-          <CommandInput placeholder="Search cities..." />
+          <CommandInput placeholder="Search cities" />
           <CommandList>
             <CommandEmpty>No city found.</CommandEmpty>
             <CommandGroup>
-              {cities.map((city) => (
+              {locations.map((l) => (
                 <CommandItem
-                  key={city.value}
-                  onSelect={(currentValue) => {
-                    setValue(currentValue === value ? "" : currentValue);
-                    setOpen(false);
-                  }}
+                  value={l.slug}
+                  key={l.slug}
+                  onSelect={handleSelect}
                 >
                   <Check
                     className={cn(
                       "mr-2 h-4 w-4",
-                      value === city.value ? "opacity-100" : "opacity-0"
+                      city === l.slug ? "opacity-100" : "opacity-0"
                     )}
                   />
-                  {city.label}
+                  {l.name}
                 </CommandItem>
               ))}
             </CommandGroup>
