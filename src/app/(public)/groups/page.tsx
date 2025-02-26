5793fdc94f9a97b172928f29be9462e19f7e696c
harshmangalam <harshdev8218@gmail.com>
2025-02-27 02:51:41 +0530

    added groups list

--- src/app/(public)/groups/page.tsx
+++ src/app/(public)/groups/page.tsx
@@ -1,3 +1,30 @@
-export default function GroupsPage() {
-  return <div></div>;
+import { getGroupList } from "@/actions/group";
+import { GroupCard } from "@/components/group-card";
+
+export default async function GroupsPage() {
+  const groups = await getGroupList({});
+  return (
+    <section className="py-12 flex flex-col gap-6 md:gap-10 container mx-auto">
+      <div className="flex flex-col gap-2 ">
+        <h2 className="text-3xl font-bold tracking-tight">
+          Explore <span className="text-destructive">Groups</span> on
+          GroupVoyage
+        </h2>
+      </div>
+
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4">
+        {groups.length ? (
+          groups.map((group) => (
+            <GroupCard
+              location={group.locations[0]}
+              key={group.id}
+              group={group}
+            />
+          ))
+        ) : (
+          <p>No groups</p>
+        )}
+      </div>
+    </section>
+  );
 }
