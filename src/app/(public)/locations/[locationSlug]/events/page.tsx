537633edff3829c1950c248a0c7e3fbd372b9c35
harshmangalam <harshdev8218@gmail.com>
2025-02-27 03:05:39 +0530

    added events page

---
+++ src/app/(public)/locations/[locationSlug]/events/page.tsx
@@ -0,0 +1,36 @@
+import { getEventList } from "@/actions/event";
+import { getLocation } from "@/actions/location";
+import { EventCard } from "@/components/event-card";
+import { notFound } from "next/navigation";
+
+type LocationEventsPageProps = {
+  params: Promise<{ locationSlug: string }>;
+};
+export default async function LocationEventssPage({
+  params,
+}: LocationEventsPageProps) {
+  const { locationSlug } = await params;
+  const location = await getLocation({ locationSlug });
+  const events = await getEventList({ locationSlug });
+
+  if (!location) return notFound();
+  return (
+    <div className=" max-w-7xl mx-auto px-4">
+      <section className="py-12 flex flex-col gap-6 md:gap-10">
+        <div className="flex flex-col gap-2">
+          <h2 className="text-3xl font-bold tracking-tight">
+            Trips from <span className="text-destructive">{location.city}</span>
+          </h2>
+        </div>
+
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+          {events.length ? (
+            events.map((event) => <EventCard key={event.id} event={event} />)
+          ) : (
+            <p>No Trips</p>
+          )}
+        </div>
+      </section>
+    </div>
+  );
+}
--- src/app/(public)/locations/[locationSlug]/groups/page.tsx
+++ src/app/(public)/locations/[locationSlug]/groups/page.tsx
@@ -3,35 +3,37 @@ import { getLocation } from "@/actions/location";
 import { GroupCard } from "@/components/group-card";
 import { notFound } from "next/navigation";
 
-type LocationGroupPageProps = {
+type LocationGroupsPageProps = {
   params: Promise<{ locationSlug: string }>;
 };
-export default async function LocationGroupsPage({
+export default async function LocationGroupssPage({
   params,
-}: LocationGroupPageProps) {
+}: LocationGroupsPageProps) {
   const { locationSlug } = await params;
   const location = await getLocation({ locationSlug });
   const groups = await getGroupList({});
 
   if (!location) return notFound();
   return (
-    <section className="py-12 flex flex-col gap-6 md:gap-10 container mx-auto">
-      <div className="flex flex-col gap-2 ">
-        <h2 className="text-3xl font-bold tracking-tight">
-          Explore Groups in{" "}
-          <span className="text-destructive">{location.city}</span>
-        </h2>
-      </div>
+    <div className=" max-w-7xl mx-auto px-4">
+      <section className="py-12 flex flex-col gap-6 md:gap-10">
+        <div className="flex flex-col gap-2 ">
+          <h2 className="text-3xl font-bold tracking-tight">
+            Explore Groups in{" "}
+            <span className="text-destructive">{location.city}</span>
+          </h2>
+        </div>
 
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4">
-        {groups.length ? (
-          groups.map((group) => (
-            <GroupCard location={location} key={group.id} group={group} />
-          ))
-        ) : (
-          <p>No groups</p>
-        )}
-      </div>
-    </section>
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4">
+          {groups.length ? (
+            groups.map((group) => (
+              <GroupCard location={location} key={group.id} group={group} />
+            ))
+          ) : (
+            <p>No groups</p>
+          )}
+        </div>
+      </section>
+    </div>
   );
 }
