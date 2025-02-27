d1a67c9fc735928a46fbe1d6824d8ee080d75efe
harshmangalam <harshdev8218@gmail.com>
2025-02-27 11:44:08 +0530

    added page section

--- src/app/(public)/groups/page.tsx
+++ src/app/(public)/groups/page.tsx
@@ -1,30 +1,32 @@
 import { getGroupList } from "@/actions/group";
 import { GroupCard } from "@/components/group-card";
+import { PageSection } from "@/components/page-section";
 
 export default async function GroupsPage() {
   const groups = await getGroupList({});
   return (
-    <section className="py-12 flex flex-col gap-6 md:gap-10 container mx-auto">
-      <div className="flex flex-col gap-2 ">
-        <h2 className="text-3xl font-bold tracking-tight">
-          Explore <span className="text-destructive">Groups</span> on
-          GroupVoyage
-        </h2>
-      </div>
-
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4">
-        {groups.length ? (
-          groups.map((group) => (
-            <GroupCard
-              location={group.locations[0]}
-              key={group.id}
-              group={group}
-            />
-          ))
-        ) : (
-          <p>No groups</p>
-        )}
-      </div>
-    </section>
+    <div className="max-w-7xl mx-auto">
+      <PageSection
+        label={
+          <span>
+            Explore <span className="text-destructive">Groups</span>
+          </span>
+        }
+      >
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4">
+          {groups.length ? (
+            groups.map((group) => (
+              <GroupCard
+                location={group.locations[0]}
+                key={group.id}
+                group={group}
+              />
+            ))
+          ) : (
+            <p>No groups</p>
+          )}
+        </div>
+      </PageSection>
+    </div>
   );
 }
--- src/app/(public)/trips/page.tsx
+++ src/app/(public)/trips/page.tsx
@@ -1,25 +1,27 @@
 import { getEventList } from "@/actions/event";
+import { PageSection } from "@/components/page-section";
 import { TripCard } from "@/components/trip-card";
 
 export default async function GroupsPage() {
   const events = await getEventList({});
 
   return (
-    <section className="py-12 flex flex-col gap-6 md:gap-10 container mx-auto">
-      <div className="flex flex-col gap-2 ">
-        <h2 className="text-3xl font-bold tracking-tight">
-          Explore <span className="text-destructive">Events</span> on
-          GroupVoyage
-        </h2>
-      </div>
-
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
-        {events.length ? (
-          events.map((event) => <TripCard key={event.id} event={event} />)
-        ) : (
-          <p>No Trips</p>
-        )}
-      </div>
-    </section>
+    <div className="max-w-7xl mx-auto">
+      <PageSection
+        label={
+          <span>
+            Explore <span className="text-destructive">Trips</span>
+          </span>
+        }
+      >
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+          {events.length ? (
+            events.map((event) => <TripCard key={event.id} event={event} />)
+          ) : (
+            <p>No Trips</p>
+          )}
+        </div>
+      </PageSection>
+    </div>
   );
 }
