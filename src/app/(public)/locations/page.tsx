c17196abddf45fd490783e1d3714289a862a76b2
harshmangalam <harshdev8218@gmail.com>
2025-03-31 08:05:50 +0530

    added locatiosn keywords

--- src/app/(public)/locations/page.tsx
+++ src/app/(public)/locations/page.tsx
@@ -1,16 +1,36 @@
 import { getLocations } from "@/actions/location";
 import { LocationCard } from "@/components/location-card";
 import { PageSection } from "@/components/page-section";
-import { SITE_NAME } from "@/lib/constants";
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: "Explore Locations",
-  description: `Find the best travel groups and compare prices from multiple departure cities for free on ${SITE_NAME}`,
-  metadataBase: new URL("https://www.groupvoyage.in/"),
+  title: "Best Weekend Trips & Getaways from Your City | Compare Prices",
+  description: `Find and compare 1-day & 2-day weekend trips from your city. Discover the best trekking, adventure, and budget-friendly group tours`,
+  keywords: [
+    "weekend trips from India",
+    "best weekend getaways in India",
+    "1-day trips from major cities",
+    "2-day budget trips from India",
+    "compare weekend travel packages",
+    "best weekend trekking groups",
+    "affordable short trips in India",
+    "local travel groups for weekend getaways",
+    "quick travel escapes from cities",
+    "top short adventure trips in India",
+    "cheap weekend group trips in India",
+    "short road trips from Indian cities",
+    "best budget weekend travel deals",
+    "find 2-day travel groups near me",
+    "short-distance trekking groups",
+    "group travel packages for weekends",
+    "explore India's best weekend trips",
+    "quick trekking and adventure getaways",
+    "local weekend tourism with price comparison",
+    "budget-friendly weekend tour options",
+  ],
 };
 export default async function LocationsPage() {
-  const locations = await getLocations({});
+  const locations = await getLocations();
   return (
     <div className="px-4 max-w-7xl mx-auto">
       <PageSection label={"Explore Locations"}>
