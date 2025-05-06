a32e876a29c8c2cff5d66cbece1e991cdccf0821
harshmangalam <harshdev8218@gmail.com>
2025-05-06 13:44:11 +0530

    removed header

--- next.config.ts
+++ next.config.ts
@@ -16,23 +16,6 @@ const nextConfig: NextConfig = {
     minimumCacheTTL: 60 * 60 * 24 * 7, // 7 days
     formats: ["image/webp"],
   },
-  async headers() {
-    return [
-      {
-        source: "/(.*)", // Apply to all routes
-        headers: [
-          {
-            key: "X-Frame-Options",
-            value: "SAMEORIGIN", // or 'DENY' if you want to block all framing
-          },
-          {
-            key: "Content-Security-Policy",
-            value: "frame-ancestors 'self'", // modern frame protection
-          },
-        ],
-      },
-    ];
-  },
 };
 
 export default nextConfig;
