8eeadb3a787dc6e5cb29b96779f911ac6dd1271c
harshmangalam <harshdev8218@gmail.com>
2025-10-24 10:59:27 +0530

    added data-scroll-behavior smooth

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -59,7 +59,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" suppressHydrationWarning>
+    <html lang="en" suppressHydrationWarning data-scroll-behavior="smooth">
       <body className={`${poppins.variable} antialiased`}>
         <NuqsAdapter>
           <ThemeProvider
